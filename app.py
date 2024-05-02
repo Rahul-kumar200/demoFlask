3eab28a0e0a381545502afaa4ff876e40ef50c6c
Rahul <rahulkr.15aug@gmail.com>
2024-05-02 09:53:43 +0530

    first comiit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask , render_template, jsonify , request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/home/')
+def home():
+    return {'msg':'this is your home'}
+
+if __name__=="__main__":
+    app.run(debug=True)
