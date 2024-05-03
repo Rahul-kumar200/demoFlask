9027ba91fe51eb4c68ae197e278af2c6bd8c709f
Rahul kumar <82836340+Rahul-kumar200@users.noreply.github.com>
2024-05-03 14:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask , render_template, jsonify , request
 from flask_cors import CORS
+import os
+
+secret_1 = os.environ.get('SECRET_1')
+secret_2 = os.environ.get('SECRET_2')
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/home/')
 def home():
-    return {'msg':'this is the home'}
+    
+    return {'msg':secret_1}
 
 if __name__=="__main__":
     app.run(debug=True)
